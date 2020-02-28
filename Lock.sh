4de411860a5709ef75acd754133a7409fd8e4f41
AL-AlamySploit <59588007+AL-AlamySploit@users.noreply.github.com>
2020-02-29 01:45:34 +0200

    Create Lock.sh

---
+++ Lock.sh
@@ -0,0 +1,13 @@
+     echo -e $Green"Enter Usernamer: "
+read user
+if [ $user == A7Y ]
+Then
+fi
+     echo -e $Green"Enter Password: "
+read pass
+if [ $pass == 1234 ]
+Then
+    echo -e $Green"welcome Admin"$user
+    exit
+fi
+       echo -e $red "Not Found.."
