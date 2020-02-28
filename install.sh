3db43250df17b2d912780cc76dd2f7769f640e27
AL-AlamySploit <59588007+AL-AlamySploit@users.noreply.github.com>
2020-02-29 01:37:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
+echo "[The code By With AL-Alamy]"
+printf "[+] witing to Down pkg ..\n"
 pkg update
 pkg install python
 pkg install figlet
 pkg install ruby
 gem install lolcat
+pkg install cmatrix
 pkg install toitet
 pkg install cowsay
-echo A7Y_Team
+echo "[-] Done.. "
