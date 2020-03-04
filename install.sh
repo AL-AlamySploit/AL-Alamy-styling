0b26252c3a8612df3583b9013d50972d21f88fcf
AL-AlamySploit <59588007+AL-AlamySploit@users.noreply.github.com>
2020-03-04 09:46:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,17 @@ echo "${A7Y}╚═╝  ╚═╝╚══════╝ ╚═╝  ╚═╝╚
 echo
 echo "[The code By With Update]"
 printf "[+] witing to Down pkg ..\n"
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+A7Y = 0
 pkg update
 pkg install python
 pkg install figlet
