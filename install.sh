9c0ab41fc5b2746a20be2bb279d983bbf7203eef
AL-AlamySploit <59588007+AL-AlamySploit@users.noreply.github.com>
2020-04-13 13:44:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,17 +10,6 @@ echo "${A7Y}╚═╝  ╚═╝╚══════╝ ╚═╝  ╚═╝╚
 echo
 echo "[The code By With Update]"
 printf "[+] witing to Down pkg ..\n"
-print "[                    ] 0% "
-time.sleep(5)
-print "[=====               ] 25%"
-time.sleep(5)
-print "[==========          ] 50%"
-time.sleep(5)
-print "[===============     ] 75%"
-time.sleep(5)
-print "[====================] 100%"
-time.sleep(3)
-A7Y = 0
 pkg update
 pkg install python
 pkg install figlet
@@ -29,4 +18,4 @@ gem install lolcat
 pkg install cmatrix
 pkg install toitet
 pkg install cowsay
-echo "[-] Done.. "
+echo "[-] Done..!"
